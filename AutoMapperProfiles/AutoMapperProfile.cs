4bccfc6248e2f9247977d0704928475d76cc1bf4
mpys_sii <mpys@sii.pl>
2020-11-18 00:26:42 +0100

    Initial commit

---
+++ AutoMapperProfiles/AutoMapperProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace WebMerchantApi.AutoMapperProfiles
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace WebMerchantApi.Data
+{
+    //public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    //{
+    //    public ApplicationDbContext(
+    //        DbContextOptions options,
+    //        IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+    //    {
+    //    }
+
+    //    //public DbSet<Product> Products { get; set; }
+
+    //    //protected override void OnModelCreating(ModelBuilder builder)
+    //    //{
+    //    //    base.OnModelCreating(builder);
+    //    //}
+    //}
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
